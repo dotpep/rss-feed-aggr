557cd987a8a07a729866d4e413e897685f37711c
dotpep <pepelnine@gmail.com>
2024-05-05 19:57:30 +0500

    Implement server with loading .env variables
    - Using github.com/joho/godotenv to load .env variables file
    - Using github.com/go-chi/chi as HTTP router service

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v\n", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port: ", portString)
+}
